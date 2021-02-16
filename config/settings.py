a6ac9ebbfa2c9dbf44b8a9c1f8669cddfe1e00fc
Rafal Szymanski <r.szymansky@gmail.com>
2021-02-16 23:03:14 +0100

    Create prod.yml

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG')
+DEBUG = env.bool('DJANGO_DEBUG', default=False )
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '.herokuapp.com', ]
 
