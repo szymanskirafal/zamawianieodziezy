a3394d988bc60984884c9a37c9d5b7569fca2303
Rafal Szymanski <r.szymansky@gmail.com>
2021-02-17 22:56:24 +0100

    Add security settings and change admin url

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
-    path('admin/', admin.site.urls),
+    path('admin-page-here/', admin.site.urls),
     # User management
     path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls')),
