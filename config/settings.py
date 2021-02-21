b36063a5390859c24c54c3d065167ae0d11a18ea
Rafal Szymanski <r.szymansky@gmail.com>
2021-02-21 21:18:10 +0100

    Change ALLOWED HOSTS to env.

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False )
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '.herokuapp.com', ]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', ])
 
 # Application definition
 
