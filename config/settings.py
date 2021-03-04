450834d154f1d4c74a3f5176e7cdb409f04bfcde
Rafal Szymanski <r.szymansky@gmail.com>
2021-03-04 22:39:40 +0100

    Add path to admin for css

--- config/settings.py
+++ config/settings.py
@@ -143,9 +143,10 @@ USE_TZ = True
 # for local development
 
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles/admin'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
