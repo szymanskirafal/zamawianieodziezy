a099d226de945495ad2e728d0451437f8fde1399
Rafal Szymanski <r.szymansky@gmail.com>
2021-04-13 22:37:44 +0200

    Change auth method to username and add home url

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('clothes/', include('clothes.urls', namespace='clothes')),
     path('employees/', include('employees.urls', namespace='employees')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('users/', include('users.urls')),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
